f8667946ff5e215f9337b4cae24adf4b8e91cd31
mk-pmb <mk-pmb@users.noreply.github.com>
2024-05-17 20:19:17 +0200

    Add vdo, installer, and a basic CI test.

---
+++ README.md
@@ -0,0 +1,31 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+github-ci-util-2405-pmb
+=======================
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+Shared tools for my GitHub CI workflows.
+<!--/#echo -->
+
+
+
+Usage
+-----
+
+See example in [`.github/workflows/ci.yaml`](.github/workflows/ci.yaml).
+
+
+
+<!--#toc stop="scan" -->
+
+
+
+&nbsp;
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ bash_funcs/ci_cli_init.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+
+
+function ghciu_cli_init_before_config () {
+  local ITEM= ADD=
+  for ITEM in "$PWD"/node_modules/.bin/; do
+    [ -d "$ITEM" ] || continue
+    ITEM="${ITEM%/}"
+    [[ ":$PATH:" == *":$ITEM:"* ]] && continue
+    ADD+="$ITEM:"
+  done
+  if [ -n "$ADD" ]; then
+    PATH="$ADD$PATH"
+    export PATH
+  fi
+
+  source_available_rc_files "$GHCIU_DIR"/cfg || return $?
+  [ . -ef "$GHCIU_DIR" ] || source_available_rc_files cfg || return $?
+}
+
+
+function in_func () {
+  "$@" || return $?$(echo W: "$FUNCNAME $* failed (rv=$?)" >&2)
+}
+
+
+function source_these_files () {
+  local ARGS="$1"; shift
+  local ITEM=
+  for ITEM in "$@"; do
+    [ -f "$ITEM" ] || [[ "$ITEM" != *'*'* ]] || continue
+    in_func source -- "$ITEM" $ARGS || return $?
+  done
+}
+
+
+function source_available_rc_files () {
+  local RC= # NB: Do not re-declare CFG!
+  for RC in "$@"; do
+    for RC in "$RC".{local,@"$HOSTNAME"}; do
+      source_these_files --config "$RC"{.,/}*.rc || return $?
+    done
+  done
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0
