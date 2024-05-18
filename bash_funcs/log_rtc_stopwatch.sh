6f22117b3bf449d6f6d640e6da5a567a70c89688
mk-pmb <mk-pmb@users.noreply.github.com>
2024-05-18 06:06:44 +0200

    Add log_rtc_stopwatch.

---
+++ bash_funcs/log_rtc_stopwatch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+
+
+function log_rtc_stopwatch () {
+  local DURA=
+  let DURA="$1"; shift
+  local MSG="$*"
+  local PRE=
+  if [[ "$MSG" == ': '* ]]; then
+    PRE="${FUNCNAME[1]}"
+    PRE="${PRE//__/: }"
+    PRE="${PRE//_/ }"
+    MSG="$PRE$MSG"
+  fi
+  MSG="${MSG//¤/$DURA}"
+  printf 'T: [%(%F %T)T] %s\n' -1 "$MSG"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0
