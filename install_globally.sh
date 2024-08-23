44c5468dbb113855ef39a6ec266708b4640a5a43
mk-pmb <mk-pmb@users.noreply.github.com>
2024-08-23 07:17:10 +0200

    Better node.js and npm setup.

--- install_globally.sh
+++ install_globally.sh
@@ -4,8 +4,8 @@
 function install_globally () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
   local SELFPATH="$(readlink -m -- "$BASH_SOURCE"/..)"
-  # cd -- "$SELFPATH" || return $?
-  local ORIG= LINK= DEST_BASE='/usr/local/bin/ghciu'
+  local GLOBAL_BIN_PATH='/usr/local/bin'
+  local ORIG= LINK= DEST_BASE="$GLOBAL_BIN_PATH/ghciu"
   [ -L "$DEST_BASE" ] && rm -v -- "$DEST_BASE"
   ln -vsT -- "$SELFPATH"/cli.sh "$DEST_BASE" || return $?
   for ORIG in "$SELFPATH"/bash_funcs/*.sh; do
@@ -14,6 +14,22 @@ function install_globally () {
     [ -L "$LINK" ] && rm -v -- "$LINK"
     ln -vsT -- "$ORIG" "$LINK" || return $?
   done
+  cd -- "$SELFPATH" || return $?
+  cp -vT -- act/install/npmrc.basics.ini "$HOME"/.npmrc || return $?
+  ensure_nodejs_symlink || return $?
 }
+
+
+function ensure_nodejs_symlink () {
+  local NJS="$(which nodejs 2>/dev/null | grep -Pe '^/')"
+  [ -x "$NJS" ] && return 0
+  local NODE="$(which node 2>/dev/null | grep -Pe '^/')"
+  [ -x "$NODE" ] || return 4$(
+    echo E: $FUNCNAME: "Cannot find an executable node.js!" >&2)
+  NJS="$GLOBAL_BIN_PATH/nodejs"
+  ln -vsT -- "$NODE" "$NJS" || return $?$(
+    echo E: $FUNCNAME: "Cannot symlink node.js as nodejs!" >&2)
+}
+
 
 install_globally "$@"; exit $?
