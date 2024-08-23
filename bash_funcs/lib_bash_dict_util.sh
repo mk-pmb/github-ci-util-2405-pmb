f18d2161734a156c7c6001c3f878bb3e1ba0a019
mk-pmb <mk-pmb@users.noreply.github.com>
2024-08-24 01:49:01 +0200

    purge-old-workflow-runs

--- bash_funcs/lib_bash_dict_util.sh
+++ bash_funcs/lib_bash_dict_util.sh
@@ -21,6 +21,40 @@ function json_to_bash_dict_init () {
 }
 
 
+function cfg_include_exclude_wordlists_prepare () {
+  local D="$1"; shift
+  local K= V=
+  while [ "$#" -ge 1 ]; do
+    K="$1"; shift
+    case "$K" in
+      *[A-Za-z0-9] ) ;;
+      * ) # Append {in,ex}clude to keys that end in a special character:
+        set -- "${K}include" "$@"; K+='exclude';;
+    esac
+    eval V='${'"$D"'[$K]}'
+    V=" ${V//$'\n'/ } "
+    [ "${V// /}" == '*' ] && V='*'
+    eval "$D"'["$K"]="$V"'
+  done
+}
+
+
+function cfg_include_exclude_wordlists_check () {
+  local D="$1" K="$2" V="$3" L=
+  [ -n "$V" ] || return 1 # invalid value
+  # local -p
+  eval L='${'"$D"'[${K}include]}'
+  [ "$L" == '*' ] || [[ "$L" == *" $V "* ]] || return 1 # not included
+  eval L='${'"$D"'[${K}exclude]}'
+  [ -z "$L" ] || [[ "$L" != *" $V "* ]] || return 1 # excluded
+  return 0 # accepted
+}
+
+
+
+
+
+
 
 
 return 0
