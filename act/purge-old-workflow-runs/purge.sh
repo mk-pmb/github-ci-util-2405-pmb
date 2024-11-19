c86eb6ba59c89206f3d8208c77e36b110c370cd5
mk-pmb <mk-pmb@users.noreply.github.com>
2024-11-19 02:19:31 +0100

    purge-old-workflow-runs: Explain and mitigate potential name confusion.

--- act/purge-old-workflow-runs/purge.sh
+++ act/purge-old-workflow-runs/purge.sh
@@ -5,35 +5,36 @@ function purge_old_wfruns () {
   local -A CFG=()
   eval "CFG=( $(json_to_bash_dict_init "$INPUTS_JSON" ) )"
   SECONDS=0
-  local PROGNAME='purge-old-workflow-runs'
+  local LOGTOPIC=' purge-old-workflow-runs:'
+  purge_old_wfruns__decide_logtopic || return $?
+  if [ "$1" == --hide-logtopic ]; then LOGTOPIC=; shift; fi
 
-  local E="E: $PROGNAME:"
-  value_mustbe_simple_integer "$E GHCIU_JOB_ID" "$GHCIU_JOB_ID" \
+  value_mustbe_simple_integer "E:$LOGTOPIC GHCIU_JOB_ID" "$GHCIU_JOB_ID" \
     ge:1 || return $?
 
   local KEY= VAL=
   local OLD="${CFG[older_than]}"
   [ -n "$OLD" ] || return 4$(
-    echo E: $PROGNAME: 'Missing option: older_than' >&2)
+    echo E:$LOGTOPIC 'Missing option: older_than' >&2)
   [ -z "${OLD//[A-Za-z0-9:. -]/}" ] || return 4$(
-    echo E: $PROGNAME: 'Option "older_than" contains unsupported characters.' \
+    echo E:$LOGTOPIC 'Option "older_than" contains unsupported characters.' \
     'Acceptable characters are letters (A-Z, a-z), digits (0-9),' \
     'colon (:), full stop (.), space and minus (-).' >&2)
   OLD="$(date +'%FT%TZ' --utc --date="$OLD")"
-  [ -n "$OLD" ] || return 4$(echo E: $PROGNAME: >&2 \
+  [ -n "$OLD" ] || return 4$(echo E:$LOGTOPIC >&2 \
     'Unsupported date/time format in option "older_than".')
-  echo D: $PROGNAME: "Effective older_than date: $OLD"
+  echo D:$LOGTOPIC "Effective older_than date: $OLD"
 
   E+=" Config error: Option"
   local OVERALL_TIMEOUT="${CFG[overall_timeout_seconds]}"
-  value_mustbe_simple_integer "$E overall_timeout_seconds" \
+  value_mustbe_simple_integer "E:$LOGTOPIC overall_timeout_seconds" \
     "$OVERALL_TIMEOUT" ge:1 || return $?
 
-  value_mustbe_simple_integer "$E api_cooldown_seconds" \
+  value_mustbe_simple_integer "E:$LOGTOPIC api_cooldown_seconds" \
     "${CFG[api_cooldown_seconds]}" ge:1 || return $?
-  value_mustbe_simple_integer "$E max_failed" \
+  value_mustbe_simple_integer "E:$LOGTOPIC max_failed" \
     "${CFG[max_failed]}" ge:0 || return $?
-  value_mustbe_simple_integer "$E max_purged" \
+  value_mustbe_simple_integer "E:$LOGTOPIC max_purged" \
     "${CFG[max_purged]}" ge:0 || return $?
 
   # Normalize the word lists
@@ -45,13 +46,13 @@ function purge_old_wfruns () {
     ;
 
   local RUNS_PER_PAGE="${CFG[api_pagesize]}"
-  value_mustbe_simple_integer "$E api_pagesize" \
+  value_mustbe_simple_integer "E:$LOGTOPIC api_pagesize" \
     "$RUNS_PER_PAGE" ge:1 || return $?
 
   local CONCLU="${CFG[conclusions]}"
-  [ -n "$CONCLU" ] || return 4$(echo "$E conclusions is empty!" >&2)
+  [ -n "$CONCLU" ] || return 4$(echo "E:$LOGTOPIC conclusions is empty!" >&2)
   [ "${CONCLU// /}" != '*' ] || return 4$(
-    echo "$E conclusions must be explicit!" >&2)
+    echo "E:$LOGTOPIC conclusions must be explicit!" >&2)
   CONCLU=" ${CONCLU} "
 
   local DATA_COLUMNS=(
@@ -73,13 +74,13 @@ function purge_old_wfruns () {
   purge_old_wfruns__download_current_runs_page || return $?
   local N_TOTAL_RUNS="$(jq .total_count -- "$CURPG_SAVE")"
   if [ "$N_TOTAL_RUNS" == 0 ]; then
-    VAL="$PROGNAME: No too-old runs were found."
+    VAL="D:$LOGTOPIC No too-old runs were found."
     echo $'\n'"$VAL"$'\n' >>"${GITHUB_STEP_SUMMARY:-/dev/null}"
     echo D: "$VAL"
     return 0
   fi
   [ "${N_TOTAL_RUNS:-0}" -ge 1 ] || return 4$(
-    echo E: $PROGNAME: 'Failed to count results.' >&2)
+    echo E:$LOGTOPIC 'Failed to count results.' >&2)
   (( N_TOTAL_PAGES = ( ( N_TOTAL_RUNS - 1 ) / RUNS_PER_PAGE ) + 1 ))
   CURPG_NUM="$N_TOTAL_PAGES"
   local N_RUNS_PURGED=0
@@ -105,16 +106,16 @@ function purge_old_wfruns () {
     (( CURPG_NUM -= 1 ))
   done
 
-  VAL="$PROGNAME: Done. $N_TOTAL_RUNS total, $N_RUNS_PURGED purged, $(
+  VAL="D:$LOGTOPIC Done. $N_TOTAL_RUNS total, $N_RUNS_PURGED purged, $(
     )$N_RUNS_FAILED failed, $N_RUNS_SKIPPED skipped."
   echo $'\n'"$VAL"$'\n' >>"${GITHUB_STEP_SUMMARY:-/dev/null}"
   echo D: "$VAL"
 }
 
 function purge_old_wfruns__api_cooldown () {
-  [ "$SECONDS" -le "$OVERALL_TIMEOUT" ] || return 4$(echo E: $PROGNAME: >&2 \
-    echo E: $PROGNAME: 'Abort: Exceeded overall_timeout_seconds.')
-  sleep "${CFG[api_cooldown_seconds]}s" || return 4$(echo E: $PROGNAME: >&2 \
+  [ "$SECONDS" -le "$OVERALL_TIMEOUT" ] || return 4$(echo E:$LOGTOPIC >&2 \
+    echo E:$LOGTOPIC 'Abort: Exceeded overall_timeout_seconds.')
+  sleep "${CFG[api_cooldown_seconds]}s" || return 4$(echo E:$LOGTOPIC >&2 \
     "Failed to let the API cool down after downloading $RUNS_URL")
 }
 
@@ -125,19 +126,19 @@ function purge_old_wfruns__download_current_runs_page () {
   CURPG_DATA="tmp.runs-pg-$CURPG_NUM.tsv"
   local RUNS_URL="$RUNS_SEARCH$CURPG_NUM"
   if [ -s "$CURPG_SAVE" ]; then
-    echo D: $PROGNAME: "We already have page $CURPG_NUM."
+    echo D:$LOGTOPIC "We already have page $CURPG_NUM."
     return 0
   fi
-  echo D: $PROGNAME: "Downloading list of too-old CI runs:" \
+  echo D:$LOGTOPIC "Downloading list of too-old CI runs:" \
     "page $CURPG_NUM of ${N_TOTAL_PAGES:-unknown}."
   wget --quiet --output-document="$CURPG_SAVE" -- "$RUNS_URL" || return $?$(
-    echo E: $PROGNAME: "Failed to download $RUNS_URL" >&2)
+    echo E:$LOGTOPIC "Failed to download $RUNS_URL" >&2)
   local JQ="${DATA_COLUMNS[*]}"
   JQ="${JQ// /, .}"
   JQ=".workflow_runs[] | [.$JQ] | @tsv"
   jq --raw-output "$JQ" -- "$CURPG_SAVE" \
     | sort --version-sort >"$CURPG_DATA" || return $?$(
-    echo E: $PROGNAME: "Failed to extract data from $RUNS_URL" >&2)
+    echo E:$LOGTOPIC "Failed to extract data from $RUNS_URL" >&2)
 }
 
 
@@ -146,10 +147,10 @@ function purge_old_wfruns__process_tsv_runs_list () {
   local BUF= KEY= VAL= WFBN= SKIP= RV= HEAD= BODY=
   while IFS= read -r BUF; do
     [ "$N_RUNS_FAILED" -lt "${CFG[max_failed]}" ] || return 4$(
-      echo E: $PROGNAME: "Abort: We have encountered $N_RUNS_FAILED" \
+      echo E:$LOGTOPIC "Abort: We have encountered $N_RUNS_FAILED" \
         "≥ max_failed = ${CFG[max_failed]} failures." >&2)
     [ "$N_RUNS_PURGED" -lt "${CFG[max_purged]}" ] || return 4$(
-      echo E: $PROGNAME: "Abort: We have purged $N_RUNS_PURGED" \
+      echo E:$LOGTOPIC "Abort: We have purged $N_RUNS_PURGED" \
         "≥ max_purged = ${CFG[max_purged]} workflow runs." >&2)
 
     purge_old_wfruns__api_cooldown || return $?
@@ -161,7 +162,7 @@ function purge_old_wfruns__process_tsv_runs_list () {
       ROW["$KEY"]="$VAL"
     done
     [ -z "$BUF" ] || return $?$(
-      echo E: $PROGNAME: "Unexpected left-over TSV column data: $BUF" >&2)
+      echo E:$LOGTOPIC "Unexpected left-over TSV column data: $BUF" >&2)
     VAL=
     local -p | grep ROW
 
@@ -184,7 +185,7 @@ function purge_old_wfruns__process_tsv_runs_list () {
     KEY="$RUNS_BASEURL/${ROW[id]}"
     if [ -n "$SKIP" ]; then
       (( N_RUNS_SKIPPED += 1 ))
-      echo D: $PROGNAME: "Skip (${SKIP#,}): $KEY"
+      echo D:$LOGTOPIC "Skip (${SKIP#,}): $KEY"
       continue
     fi
 
@@ -192,8 +193,8 @@ function purge_old_wfruns__process_tsv_runs_list () {
     RV=$?
     BUF="${BUF//$'\r'/}"
     if [ "$RV" != 0 ]; then
-      echo W: $PROGNAME: "Failed to delete $KEY (rv=$RV)" >&2
-      echo W: $PROGNAME: "API response as base64: $(
+      echo W:$LOGTOPIC "Failed to delete $KEY (rv=$RV)" >&2
+      echo W:$LOGTOPIC "API response as base64: $(
         base64 --wrap=9009009009 <<<"$BUF")" >&2
       (( N_RUNS_FAILED += 1 ))
       continue
@@ -204,30 +205,49 @@ function purge_old_wfruns__process_tsv_runs_list () {
     BODY="${BUF#*$'\n\n'}"
 
     if [ "${HEAD:0:11}" == 'HTTP/2 404 ' ]; then
-      echo D: $PROGNAME: "Missing in action: $KEY"
+      echo D:$LOGTOPIC "Missing in action: $KEY"
       (( N_RUNS_PURGED += 1 ))
       continue
     fi
     if [ -z "$BODY" -a "${HEAD:0:11}" == 'HTTP/2 204 ' ]; then
-      echo D: $PROGNAME: "Deleted: $KEY"
+      echo D:$LOGTOPIC "Deleted: $KEY"
       (( N_RUNS_PURGED += 1 ))
       continue
     fi
 
     case "$BODY" in
       '{'*'"message":'*'}'* )
-        echo E: $PROGNAME: 'Unexpected API reply:' \
+        echo E:$LOGTOPIC 'Unexpected API reply:' \
           "[${HEAD%%$'\n'*}] ${BODY//$'\n'/ }" >&2
         return 4;;
     esac
 
-    echo E: $PROGNAME: "Unexpected API reply" >&2
-    echo W: $PROGNAME: "API response headers (${#HEAD} bytes) as base64: $(
+    echo E:$LOGTOPIC "Unexpected API reply" >&2
+    echo W:$LOGTOPIC "API response headers (${#HEAD} bytes) as base64: $(
       base64 --wrap=9009009009 <<<"$HEAD")" >&2
-    echo W: $PROGNAME: "API response body (${#BODY} bytes) as base64: $(
+    echo W:$LOGTOPIC "API response body (${#BODY} bytes) as base64: $(
       base64 --wrap=9009009009 <<<"$BODY")" >&2
     return 4
   done
+}
+
+
+function purge_old_wfruns__decide_logtopic () {
+  local WF="$GITHUB_WORKFLOW"
+  WF="${WF,,}"
+  WF="${WF//[_.-]/}"
+  # echo D: $FUNCNAME: "WF='$WF'"
+  case "$WF" in
+    */clean*oldworkflow* | \
+    */pruneoldworkflow* | \
+    */purgeoldworkflow* | \
+    */wipeoldworkflow* | \
+    . )
+      # Workflows with names like these probably won't do much else,
+      # so there's no need to include a (potentially confusing because
+      # different) topic in log messages.
+      LOGTOPIC=;;
+  esac
 }
 
 
