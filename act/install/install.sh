6b548ad076fc3dc4bb462edef440b14a14ff091a
mk-pmb <mk-pmb@users.noreply.github.com>
2026-04-06 17:31:19 +0200

    Verify the installed node.js version.

--- act/install/install.sh
+++ act/install/install.sh
@@ -6,6 +6,31 @@ function install_ghciu () {
   local REPOPATH="$(readlink -m -- "$BASH_SOURCE"/../../..)"
   "$REPOPATH"/install_globally.sh || return $?
   ghciu ghciu+s://act/install/gather_ci_run_meta.sh || return $?
+  ghciu_verify_nodejs_version || return $?
+}
+
+
+function ghciu_verify_nodejs_version () {
+  local WANT="$WANT_NODEJS_VERSION"
+  local HAVE="$(nodejs --version | grep -xPe 'v?\d[\d\.]+')"
+  local MAJOR="${HAVE%%.*}"
+  MAJOR="${MAJOR#v}"
+  echo D: $FUNCNAME: "Found '$HAVE' => major = $MAJOR"
+  local ERR=
+  [ "${MAJOR:-0}" -ge 1 ] || ERR='Cannot determine the installed %NV!'
+  [ -n "$ERR" ] || case "$WANT" in
+    '' ) return 0;;
+    *[^0-9]* ) ERR='%RNV contains a non-digit character!';;
+    "$MAJOR" ) return 0;;
+    [1-9]* ) ERR="%RNV is $WANT.* but we have $HAVE";;
+    * ) ERR='%RNV must be a simple positive integer';;
+  esac
+  [ -n "$ERR" ] || ERR='Unexpected control flow!'
+  ERR="${ERR//%RNV/Requested %NV}"
+  ERR="${ERR//%NV/node.js version}"
+  echo E: $FUNCNAME: "$ERR" >&2
+  printf -- '\n### E: `%s:` %s\n\n' "$FUNCNAME" "$ERR" >>"$GITHUB_STEP_SUMMARY"
+  return 4
 }
 
 
